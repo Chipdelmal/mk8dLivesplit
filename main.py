2ced12cd8bf93b352415fd2b2c31f76ac3b9d70b
chipdelmal <chipdelmal@gmail.com>
2020-05-23 19:42:33 -0700

    Improbements to violin plot

--- main.py
+++ main.py
@@ -26,12 +26,19 @@ for track in range(len(segment)):
 
 
 # Create a figure instance
-fig = plt.figure()
+fig = plt.figure(figsize=(24, 12))
 # Create an axes instance
 ax = fig.add_axes([0, 0, 1, 1])
 ax.set_ylim(80, 150)
 ax.set_xticks(range(1, len(tNames) + 1))
 ax.set_xticklabels(tNames, rotation=90)
+major_ticks = range(1, 48+1, 1)
+minor_ticks = range(1, 48+1, 4)
+ax.grid(which='both')
+ax.set_xticks(major_ticks)
+ax.set_xticks(minor_ticks, minor=True)
+ax.grid(which='minor', alpha=1)
+ax.grid(which='major', alpha=.1)
 # Create the boxplot
-bp = ax.violinplot(tHists)
-plt.show()
+bp = ax.violinplot(tHists, widths=1)
+fig.savefig('./img/violin.png', pad_inches=.1, bbox_inches="tight")
