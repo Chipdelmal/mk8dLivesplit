c764b62e1e78c2dcf11ca1826888d223e817c5c2
chipdelmal <chipdelmal@gmail.com>
2020-08-02 19:44:39 -0700

    Starting violin plot

--- dev.py
+++ dev.py
@@ -1,24 +1,66 @@
 
 import dataFunctions as fun
-from collections import OrderedDict
+import matplotlib.pylab as pl
+import matplotlib.pyplot as plt
 
 (PATH, OUT, FILE) = (
         './dta/', '/home/chipdelmal/MEGAsync/MK8D/',
         'Mario Kart 8 Deluxe - 48 Tracks (200cc, Digital, No Items).lss'
 
     )
-# Parse XML ###################################################################
+###############################################################################
+# Read File
+###############################################################################
 seg = fun.getSegmentsFromFile(PATH+FILE)
-# Runs history and stats ######################################################
+###############################################################################
+# Shape Runs Data
+###############################################################################
+# Runs history and stats ------------------------------------------------------
 runsHistory = fun.getRunsDict(seg)
 runsStats = fun.getRunsStats(runsHistory)
-# Filter to finished runs #####################################################
+# Filter to finished runs -----------------------------------------------------
 fshdRunID = fun.getFinishedRunsId(seg)
 fshdRunHistory = fun.filterRunsDict(runsHistory, fshdRunID)
 fshdRunsStats = fun.getRunsStats(fshdRunHistory)
-# Cumulative history ##########################################################
+# Cumulative history ----------------------------------------------------------
 fshdRunHistoryCml = fun.calcRunsCumulative(fshdRunHistory)
 fshdRunsCmlStats = fun.getRunsStats(fshdRunHistoryCml)
-# Filter runs #################################################################
+# Filter runs -----------------------------------------------------------------
 trace = fun.getRunFromID(fshdRunHistoryCml, 66)
-runsHistory
+###############################################################################
+# Plot Data
+###############################################################################
+# IN --------------------------------------------------------------------------
+# runsHistory
+# runsStats
+ylim = (70, 150)
+meanStyle = {'color': (.3, .3, .3), 'alpha': .8, 'width': .8}
+medianStyle = {'color': (.3, .3, .3), 'alpha': .75, 'width': .75}
+# Preprocess ------------------------------------------------------------------
+tNames = list(runsHistory.keys())
+tNum = len(tNames)
+tTimes = [list(runsHistory[track].values()) for track in tNames]
+# Setup figure and axes -------------------------------------------------------
+fig = plt.figure(figsize=(24, 12))
+ax = fig.add_axes([0, 0, 1, 1])
+ax.set_ylim(ylim[0], ylim[1])
+ax.set_xlim(-1, tNum + 2)
+# Violin plot -----------------------------------------------------------------
+bp = ax.violinplot(
+        tTimes, widths=.9,
+        showmedians=True, showmeans=True, showextrema=False
+    )
+# Medians and Means style -----------------------------------------------------
+vp = bp['cmedians']
+vp.set_edgecolor(medianStyle['color'])
+vp.set_linewidth(medianStyle['width'])
+vp.set_alpha(medianStyle['alpha'])
+vp = bp['cmeans']
+vp.set_edgecolor(meanStyle['color'])
+vp.set_linewidth(meanStyle['width'])
+vp.set_alpha(meanStyle['alpha'])
+# Grids -----------------------------------------------------------------------
+ax.grid(which='both')
+major_ticks = range(1, tNum+1, 1)
+ax.set_xticks(major_ticks)
+ax.set_xticklabels(tNames, rotation=90)
