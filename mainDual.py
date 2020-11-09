82df0d4cdbe0faa4f7936d568ee353f98edd7ffd
chipdelmal <chipdelmal@gmail.com>
2020-11-08 19:40:17 -0800

    Plotly violin is working

--- mainDual.py
+++ mainDual.py
@@ -14,15 +14,14 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 
-
 # (FILE, OUT, VOFF) = (sys.argv[1], sys.argv[2], sys.argv[3])
 # (DPI, PAD, TYP, VOFF) = (250, .1, 'png', 30)
 TYP = 'png'
 (FILE_D, FILE_C, OUT, VOFF) = (
-        './dta/Mario Kart 8 Deluxe - 48 Tracks (200cc, Digital, No Items).lss',
-        './dta/Mario Kart 8 Deluxe - 48 Tracks (200cc, Cartridge, No Items).lss',
-        '/home/chipdelmal/Documents/MK8D/Livesplit/', 35
-    )
+    './dta/Mario Kart 8 Deluxe - 48 Tracks (200cc, Digital, No Items).lss',
+    './dta/Mario Kart 8 Deluxe - 48 Tracks (200cc, Cartridge, No Items).lss',
+    '/home/chipdelmal/Documents/MK8D/Livesplit/', 35
+)
 ###############################################################################
 # Read File
 ###############################################################################
@@ -66,15 +65,30 @@ for hist in range(len(runsHS)):
 # aux.saveFig(fig, '{}violinSplits.{}'.format(OUT, TYP), dpi=500)
 # Plot ------------------------------------------------------------------------
 fig = go.Figure()
-fig.add_trace(go.Violin(x=df['Track'][ df['Version'] == 'Digital' ],
-                        y=df['Time'][ df['Version'] == 'Digital' ],
-                        legendgroup='Yes', scalegroup='Yes', name='Yes',
-                        side='negative',
-                        line_color='blue')
-             )
-fig.add_trace(go.Violin(x=df['Track'][ df['Version'] == 'Cartridge' ],
-                        y=df['Time'][ df['Version'] == 'Cartridge' ],
-                        legendgroup='No', scalegroup='No', name='No',
-                        side='positive',
-                        line_color='orange')
-             )
+fig.add_trace(go.Violin(
+    x=df['Track'][df['Version']== ver[0]],
+    y=df['Time'][df['Version']==ver[0]],
+    legendgroup=ver[0], 
+    scalegroup=ver[0], name=ver[0], side='negative',
+    line_color='blue', points=False
+))
+fig.add_trace(go.Violin(
+    x=df['Track'][df['Version']==ver[1]],
+    y=df['Time'][df['Version']==ver[1]],
+    legendgroup=ver[1], 
+    scalegroup=ver[1], name=ver[1], side='positive',
+    line_color='purple', points=False
+))
+fig.update_traces(
+    orientation='v', 
+    meanline_visible=True, width=1, points=False
+)
+fig.update_layout(
+    violingap=.5, violinmode='overlay', 
+    autosize=True, width=1250, height=500,
+    margin=go.layout.Margin(l=1, r=1, b=1, t=1, pad=0)
+)
+fig.update_xaxes(range=[-.5, len(tracks)-.5])
+# fig.show()
+fig.write_html('{}violins.html'.format(OUT))
+
